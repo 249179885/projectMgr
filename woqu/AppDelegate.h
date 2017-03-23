bb6c6c70421531a5e6d65482c2600373968feda0
zhangshuying <zhangshuying@zhangshuyingdeMacBook-Air.local>
2017-03-23 11:34:50 +0800

    Initial Commit

---
+++ woqu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  woqu
+//
+//  Created by zhangshuying on 2017/3/23.
+//  Copyright © 2017年 zhangshuying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ woqu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  woqu
+//
+//  Created by zhangshuying on 2017/3/23.
+//  Copyright © 2017年 zhangshuying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
